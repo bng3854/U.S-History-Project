6d9d6bf0ec8e72d0b859f582d8c04b3459cce6c6
bng3854 <96214234+bng3854@users.noreply.github.com>
2023-01-12 18:17:59 -0500

    Create QuestionsAndAnswers.cs

---
+++ QuestionsAndAnswers.cs
@@ -0,0 +1,19 @@
+
+[System .Serializable]
+public class QuestionsAndAnswers 
+{
+    public string Question;
+    public string[] Answers;
+    public int correctAnswer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
